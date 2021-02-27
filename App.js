c3cef3f9a1991f63562dfe46a5a402ba7e3747c6
노승철 <s_chuli@noseungcheol-ui-MacBookAir.local>
2021-02-27 21:37:48 +0900

    commit

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import Weather from './src/component/Weather';
+
+const App = () => {
+  return (    
+      <Weather />    
+  )
+}
+
+export default App;
